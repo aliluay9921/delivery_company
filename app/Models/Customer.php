bead2e8f27d01b0c076b90970effdac4211507e8
aliluay992 <luaya577@gmail.com>
2021-10-01 02:47:26 +0300

    ali

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -12,7 +12,7 @@ class Customer extends Model
     use HasFactory, Uuids, SoftDeletes;
     protected $guarded = [];
     protected $dates = ['deleted_at'];
-    // protected $appends = ['Balance'];
+    protected $appends = ['Balance'];
 
     public function goods_recevied()
     {
@@ -22,15 +22,14 @@ class Customer extends Model
     public function getBalanceAttribute()
     {
         $price = 0;
-        $goods = $this->goods_recevied()->where('order_status', 1)->get();
+        // $goods = $this->goods_recevied()->where('order_status', 1)->get();
+        $goods = GoodReceived::where('customer_id', $this->id)->where('order_status', 1)->get();
         foreach ($goods as $good) {
             $price += $good->price;
+            if ($good->type_deliver == 0) {
+                $price -= $good->delevery_price->company_cost + $good->delevery_price->driver_cost;
+            }
         }
-        $min = $this->goods_recevied()->where('order_status', 1)->where('type_deliver', 0)->get();
-        foreach ($min as $num) {
-            $price -= $num->delevery_price()->company_cost + $min->delevery_price()->driver_cost;
-        }
-        // delevery_price
         return  $price;
     }
 }
