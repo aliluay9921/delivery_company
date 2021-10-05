d768c65e8113ad683ddfb7cfa35d5940503d042a
aliluay992 <luaya577@gmail.com>
2021-10-05 21:15:57 +0300

    ali

--- app/Http/Controllers/IncomesController.php
+++ app/Http/Controllers/IncomesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Log;
 use App\Models\Income;
 use App\Traits\Pagination;
 use App\Traits\SendResponse;
@@ -29,15 +30,24 @@ class IncomesController extends Controller
             return $this->send_response(401, 'خطأ بالمدخلات', $validator->errors(), []);
         }
         $data = [];
+        $log = [];
         $data = [
             'type' => $request['type'],
             'name' => $request['name'],
             'value' => $request['value'],
         ];
+        $log = [
+            'value' => $request['value'],
+            'log_type' =>  $request['name'],
+            'type' => $request['type'],
+            'user_id'   => auth()->user()->id,
+        ];
         if (array_key_exists('customer_id', $request)) {
             $data['customer_id'] = $request['customer_id'];
+            $log['target_id'] = $request['customer_id'];
         }
         $income = Income::create($data);
+        Log::create($log);
         return $this->send_response(200, 'تم اضافة قيمة الى القاصة ', [], Income::find($income->id));
     }
 }
