e2aed6330b11d581eacb897b1120699d199013ab
aliluay992 <luaya577@gmail.com>
2021-10-06 15:10:25 +0300

    ali

--- database/migrations/2021_09_24_084700_create_user_permissions_table.php
+++ database/migrations/2021_09_24_084700_create_user_permissions_table.php
@@ -14,7 +14,7 @@ class CreateUserPermissionsTable extends Migration
     public function up()
     {
         Schema::create('user_permissions', function (Blueprint $table) {
-            $table->id()->primary();
+            $table->id();
             $table->uuid('user_id');
             $table->uuid('permission_id');
             $table->timestamps();
